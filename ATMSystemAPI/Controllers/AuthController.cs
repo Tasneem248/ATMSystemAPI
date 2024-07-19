68b486d14f0aa7023038635e17a0c8bd784feca1
TASNIM <tasnim.fattal@arafas.com>
2024-07-19 17:02:53 +0300

    https://github.com/Tasneem248/ATMSystemAPI

--- ATMSystemAPI/Controllers/AuthController.cs
+++ ATMSystemAPI/Controllers/AuthController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿/*using Microsoft.AspNetCore.Mvc;
 using ATMSystemAPI.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,13 +29,64 @@ namespace ATMSystemAPI.Controllers
             return Ok(new { Token = token });
         }
 
-        /*[HttpGet("[action]")]
-        public IActionResult GetLoggedUser()
-        {
-            string token = HttpContext.Request.Headers["Authorization"].ToString() ?? "";
-            var user = Common.Users.FirstOrDefault(x => x.Token.Equals(token));
+      
+    }
+}*/
 
-            return Ok(new { Name = user.Username });
-        }*/
+using Microsoft.AspNetCore.Mvc;
+using ATMSystemAPI.Models;
+using ATMSystemAPI.Data;
+using System.Linq;
+using System.Text;
+using System.IO;
+using ATMSystemAPI.Helpers;
+
+namespace ATMSystemAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        // Login metodu
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] User newUser)
+        {
+            if (Common.Users.Any(u => u.Username == newUser.Username))
+            {
+                return BadRequest("Bu kullanıcı adı zaten kullanılıyor.");
+            }
+
+            newUser.Id = Common.Users.Count > 0 ? Common.Users.Max(u => u.Id) + 1 : 1;
+            Common.Users.Add(newUser);
+            UserFileHelper.WriteUsersToFile(Common.Users);
+
+            return Ok("Kullanıcı başarıyla kaydedildi.");
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] User login)
+        {
+            var user = Common.Users.SingleOrDefault(u => u.Username == login.Username && u.Password == login.Password);
+
+            if (user == null)
+            {
+                return Unauthorized("Kullanıcı adı veya şifre hatalı");
+            }
+
+            // Özel token oluşturma
+            string token = TokenGenerator.GenerateCustomToken(64);
+            user.Token = token; // Kullanıcının token'ını güncelle
+            UserFileHelper.WriteUsersToFile(Common.Users);
+
+            return Ok(new { Token = token });
+        }
+
+        [HttpGet("read-users")]
+        public IActionResult ReadUsers()
+        {
+            var users = Common.Users;
+            return Ok(users);
+        }
     }
 }
---
+++ ATMSystemAPI/Helpers/UserFileHelper.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using ATMSystemAPI.Models;
+
+namespace ATMSystemAPI.Helpers
+{
+    public static class UserFileHelper
+    {
+        private static string filePath = "users.txt";
+
+        public static List<User> ReadUsersFromFile()
+        {
+            var users = new List<User>();
+            if (!File.Exists(filePath))
+            {
+                return users;
+            }
+
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines)
+            {
+                var parts = line.Split(',');
+                if (parts.Length == 3)
+                {
+                    users.Add(new User
+                    {
+                        Username = parts[0].Split(':')[1],
+                        Password = parts[1].Split(':')[1],
+                        Token = parts[2].Split(':')[1]
+                    });
+                }
+            }
+            return users;
+        }
+
+        public static void WriteUsersToFile(List<User> users)
+        {
+            var lines = users.Select(u => $"username:{u.Username},password:{u.Password},token:{u.Token}");
+            File.WriteAllLines(filePath, lines);
+        }
+
+        public static void UpdateUserToken(string username, string token)
+        {
+            var users = ReadUsersFromFile();
+            var user = users.SingleOrDefault(u => u.Username == username);
+            if (user != null)
+            {
+                user.Token = token;
+                WriteUsersToFile(users);
+            }
+        }
+    }
+}
